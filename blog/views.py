d5e0ea21bde6a67cfeb52d09e9429c56c55bba56
Backyardigans34 <victor.sousa5@estudante.ifto.edu.br>
2023-04-03 16:24:46 -0300

    alteração base

--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,15 @@ from django.shortcuts import redirect
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def post_new(request):
-    [...]
+
+
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
